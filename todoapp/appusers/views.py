06ada08842ab3df028d38ee5532228032715b436
Ivan <ivan.pavlov.alk@gmail.com>
2021-12-26 16:48:35 +0200

    Lesson 7 - Completed

--- todoapp/appusers/views.py
+++ todoapp/appusers/views.py
@@ -1,11 +1,12 @@
 from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
 from rest_framework.viewsets import GenericViewSet
-from rest_framework import mixins
+from rest_framework import mixins, permissions
 from .models import AppUsers
 from .serializer import AppUserSerializer
 
 
 class AppUserViewSet(mixins.UpdateModelMixin, mixins.ListModelMixin, mixins.RetrieveModelMixin, GenericViewSet):
     # renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
+    permissions_classes = [permissions.IsAuthenticated]
     queryset = AppUsers.objects.all()
     serializer_class = AppUserSerializer
--- todoapp/frontend/src/App.js
+++ todoapp/frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import axios from 'axios';
-import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom'
+import {BrowserRouter, Route, Switch, Redirect, Link} from 'react-router-dom'
 import UsersList from './components/Appusers.js'
 import ProjectList from './components/Projects.js'
 import KanbanList from './components/Kanban.js'
 import ProjectDetail from './components/ProjectDetail.js'
+import LoginForm from './components/Auth.js'
+import Cookies from 'universal-cookie'
 //import logo from './logo.svg';
 //import './App.css';
 
@@ -24,51 +26,120 @@ class App extends React.Component {
             'appusers': [],
             'projects': [],
             'kanbans': [],
+            'token': '',
+
         }
     }
 
+    set_token(token) {
+        const cookies = new Cookies()
+        cookies.set('token', token)
+        this.setState({token: token}, () => this.load_data())
+    }
+
+    is_authenticated() {
+        return this.state.token !== ''
+    }
+
+    logout() {
+        this.set_token('')
+    }
+
+    get_token_from_storage() {
+        const cookies = new Cookies()
+        const token = cookies.get('token')
+        this.setState({'token': token}, () => this.load_data())
+    }
+
+    get_token(username, password) {
+        axios
+            .post('http://127.0.0.1:8000/api-token-auth/', {
+                username: username,
+                password: password
+            })
+            .then(response => {
+                this.set_token(response.data['token'])
+                console.log(this.state.token)
+            })
+            .catch(error => alert('Wrong username or password'))
+    }
+
+    get_headers() {
+        let headers = {'Content-Type': 'application/json'};
+        if (this.is_authenticated()) {
+            headers['Authorization'] = 'Token' + this.state.token
+        }
+        return headers
+    }
+
+
+    load_data() {
+        const headers = this.get_headers()
+        axios
+            .get('http://127.0.0.1:8000/api/users', {headers})
+
+            .then(response => {
+                const users = response.data.results
+                this.setState(
+                    {
+                        'users': users
+                    }
+                )
+            })
+            .catch(error => {
+                console.log(error)
+                this.setState({users: []})
+            })
+
+
+        axios
+            .get('http://127.0.0.1:8000/api/projects', {headers})
+            .then(response => {
+                const projects = response.data.results
+                this.setState(
+                    {
+                        'projects': projects
+                    }
+                )
+            })
+            .catch(error => console.log(error))
+
+
+        axios
+            .get('http://127.0.0.1:8000/api/todos', {headers})
+            .then(response => {
+                const todos = response.data.results
+                this.setState(
+                    {
+                        'todos': todos
+                    }
+                )
+            })
+            .catch(error => console.log(error))
+    }
+
     componentDidMount() {
-        axios.get('http://127.0.0.1:8000/api/appusers/')
-       .then(response => {
-           const appusers = response.data.results
-               this.setState(
-               {
-                   'appusers': appusers
-               }
-           )
-       }).catch(error => console.log(error))
-
-       axios.get('http://127.0.0.1:8000/api/project/')
-       .then(response => {
-           const projects = response.data.results
-               this.setState(
-               {
-                   'projects': projects
-               }
-           )
-       }).catch(error => console.log(error))
-
-       axios.get('http://127.0.0.1:8000/api/kanban/')
-       .then(response => {
-           const kanbans = response.data.results
-               this.setState(
-               {
-                   'kanbans': kanbans
-               }
-           )
-       }).catch(error => console.log(error))
-}
+        this.get_token_from_storage()
+    }
 
     render() {
         return (
+
+
         <div>
         <BrowserRouter>
+            {this.is_authenticated() ?
+            <button onClick={() => this.logout()}>Logout</button> :
+            <Link to='/login'>Login</Link>
+        }
             <Switch>
+
                 <Route exact path='/' component={() => <ProjectList projects={this.state.projects}/>}/>
                 <Route exact path='/kanban' component={() => <KanbanList kanbans={this.state.kanbans}/>}/>
                 <Route exact path='/appusers' component={() => <UsersList appusers={this.state.appusers}/>}/>
-                 <Route exact path='/project/:id'>
-                    <ProjectDetail projects={this.state.projects} appusers={this.state.appusers} kanbans={this.state.kanbans} />
+                <Route exact path='/project/:id'>
+                   <ProjectDetail projects={this.state.projects} appusers={this.state.appusers} kanbans={this.state.kanbans} />
+                <Route exact path='/login' component={() => <LoginForm get_token={(username, password) => this.get_token(username, password)}/>}/>
                 </Route>
                 <Redirect from='/projects' to='/'/>
                 <Route component={NotFound404}/>
@@ -81,29 +152,5 @@ class App extends React.Component {
     }
 
 }
-
-
-
-
-//function App() {
-//  return (
-//    <div className="App">
-//      <header className="App-header">
-//        <img src={logo} className="App-logo" alt="logo" />
-//        <p>
-//          Edit <code>src/App.js</code> and save to reload.
-//        </p>
-//        <a
-//          className="App-link"
-//          href="https://reactjs.org"
-//          target="_blank"
-//          rel="noopener noreferrer"
-//        >
-//          Learn React
-//        </a>
-//      </header>
-//    </div>
-//  );
-//}
 
 export default App;
